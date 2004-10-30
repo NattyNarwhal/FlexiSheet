39ccca29484d9575c6a52e54fe9102ed09ff9637
flexisheet <>
2004-10-30 08:19:31 +0000

    *** empty log message ***

---
+++ Functions/FSLogFunction.m
@@ -0,0 +1,94 @@
+//  $Id$
+//
+//  FSLogFunction.m
+//  FlexiSheet
+//
+//  Created by Stefan Leuker on 06-OCT-2001.
+//
+//  Copyright (c) 2001-2004, Stefan Leuker.        All rights reserved.
+//  
+//  Redistribution and use in source and binary forms,  with or without
+//  modification,  are permitted provided that the following conditions
+//  are met:
+//  
+//  *  Redistributions of source code must retain the above copyright
+//     notice,  this list of conditions and the following disclaimer.
+//  
+//  *  Redistributions  in  binary  form  must  reproduce  the  above
+//     copyright notice,  this  list of conditions  and the following
+//     disclaimer  in  the  documentation  and / or  other  materials
+//     provided with the distribution.
+//  
+//  *  Neither the name  "FlexiSheet"  nor the names of its copyright
+//     holders  or  contributors  may  be used  to endorse or promote
+//     products  derived  from  this software  without specific prior
+//     written permission.
+//  
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+//  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,  INCLUDING,  BUT NOT
+//  LIMITED TO,  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND  FITNESS
+//  FOR  A PARTICULAR PURPOSE  ARE  DISCLAIMED.  IN NO EVENT  SHALL THE
+//  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+//  INCIDENTAL, SPECIAL, EXEMPLARY OR CONSEQUENTIAL DAMAGES (INCLUDING,
+//  BUT NOT LIMITED TO,  PROCUREMENT  OF  SUBSTITUTE GOODS OR SERVICES;
+//  LOSS OF USE,  DATA,  OR PROFITS;  OR BUSINESS INTERRUPTION) HOWEVER
+//  CAUSED AND ON ANY THEORY OF LIABILITY,  WHETHER IN CONTRACT, STRICT
+//  LIABILITY, OR TORT  (INCLUDING NEGLIGENCE OR OTHERWISE)  ARISING IN
+//  ANY WAY  OUT  OF  THE USE OF THIS SOFTWARE,  EVEN IF ADVISED OF THE
+//  POSSIBILITY OF SUCH DAMAGE.
+//  
+
+#import "FSLogFunction.h"
+#include <math.h>
+
+@implementation FSLogFunction
+
++ (void)initialize
+{
+    [FSFunction registerFunction:self];
+}
+
++ (NSString*)functionName
+{
+    return @"log";
+}
+
++ (int)numberOfOperands
+{
+    return 1;
+}
+
+- (id)initWithArgument:(FSExpression*)argument
+{
+    self = [super init];
+    if (self) {
+        _argument = [argument retain];
+    }
+    return self;
+}
+
+- (void)dealloc
+{
+    [_argument release];
+    [super dealloc];
+}
+
++ (FSFunction*)functionWithArguments:(NSArray*)arguments
+{
+    return [[[self alloc] initWithArgument:[arguments objectAtIndex:0]]
+        autorelease];
+}
+
+- (id)formulaValueForKeySet:(FSKeySet*)fs
+/*" Returns the log of _argument. "*/
+{
+    return [NSNumber numberWithDouble:log10([[_argument formulaValueForKeySet:fs] doubleValue])];
+}
+
+- (NSString*)creatorString
+{
+    return [NSString stringWithFormat:@"%@(%@)", 
+        [[self class] functionName], [_argument creatorString]];
+}
+
+@end
